e8af88370b6b6560f2d909e66b5c0dedd88b8dd2
Cheng <lechenggood@foxmail.com>
2017-03-05 13:42:04 +0800

    第一次提交ReactNative

---
+++ setup.js
@@ -0,0 +1,59 @@
+/**
+ * Created by Cheng on 2017/3/5.
+ */
+/**
+ * Sample React Native App
+ * https://github.com/facebook/react-native
+ * @flow
+ * Terminal:
+ * react-native run-android         //运行在安卓
+ */
+
+import React, {Component} from 'react';
+import {
+    AppRegistry,
+    StyleSheet,
+    Text,
+    View
+} from 'react-native';
+import HelloComponent from './MyComponent';
+
+export default class setup extends Component {
+    render() {
+        return (
+            <View style={styles.container}>
+                <Text style={styles.welcome}>
+                    Welcome to React Native!
+                </Text>
+                <Text style={styles.instructions}>
+                    To get started, edit index.ios.js
+                </Text>
+                <Text style={styles.instructions}>
+                    Press Cmd+R to reload,{'\n'}
+                    Cmd+D or shake for dev menu
+                </Text>
+                <HelloComponent/>
+            </View>
+        );
+    }
+}
+//
+const styles = StyleSheet.create({
+    container: {
+        flex: 1,
+        backgroundColor: '#1f82bb',
+        marginTop: 50,
+    },
+    welcome: {
+        fontSize: 20,
+        textAlign: 'center',
+        margin: 10,
+    },
+    instructions: {
+        textAlign: 'center',
+        color: '#333333',
+        marginBottom: 5,
+    },
+
+});
+
