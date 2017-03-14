466ce2ff8242a1f29cf452767cdacedd5794e07a
公司 <951827860@qq.com>
2017-03-14 08:37:12 +0800

    增加了TableView.js , 公司第一次提交，

---
+++ js/component/TableView.js
@@ -0,0 +1,22 @@
+/**
+ * Created by 乐城 on 2017/3/13.
+ * https://www.npmjs.com/package/react-native-scrollable-tab-view
+ * Run npm install react-native-scrollable-tab-view --save
+ * var ScrollableTabView = require('react-native-scrollable-tab-view');
+ *
+ *
+ *
+ var ScrollableTabView = require('react-native-scrollable-tab-view');
+
+ var App = React.createClass({
+  render() {
+    return (
+      <ScrollableTabView>
+        <ReactPage tabLabel="React" />
+        <FlowPage tabLabel="Flow" />
+        <JestPage tabLabel="Jest" />
+      </ScrollableTabView>
+    );
+  }
+});
+ */
