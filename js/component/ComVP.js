fd0d81547300411959163ce2ec36f74918d64877
公司 <951827860@qq.com>
2017-04-25 14:07:54 +0800

    zen增加了Viewpager的模块，还未测好。

--- js/component/ComVP.js
+++ js/component/ComVP.js
@@ -10,48 +10,50 @@ import {
     View,
     WebView,
     Dimensions,
+    ListView,
     Text,
 } from 'react-native';
 var ViewPager = require('react-native-viewpager');
-// import ScrollableTabView, {DURATION} from 'react-native-scrollable-tab-view'
+import Toast, {DURATION} from 'react-native-easy-toast'
+
 export default class ComVP extends Component {
 
-    constructor(props) {
-        super(props);
-        var ds = new ListView.DataSource({rowHasChanged: (r1, r2) => r1 !== r2});
-        this.state = {
-            dataSource: ds.cloneWithRows(this._genRows()),
-        };
-    }
-
-    _genRows() {
-        const dataBlob = [];
-        for (let i = 0; i < 5; i++) {
-            dataBlob.push("aa" + i);
-        }
-        return dataBlob;
-    }
-
-    render() {
-        return
-        <ViewPager
-            dataSource={this.state.dataSource}
-            renderPage={this._renderPage}
-            animation={(animatedValue, toValue, gestureState) => {
-                // Use the horizontal velocity of the swipe gesture
-                // to affect the length of the transition so the faster you swipe
-                // the faster the pages will transition
-                var velocity = Math.abs(gestureState.vx);
-                var baseDuration = 300;
-                var duration = (velocity > 1) ? 1 / velocity * baseDuration : baseDuration;
-
-                return Animated.timing(animatedValue,
-                    {
-                        toValue: toValue,
-                        duration: duration,
-                        easing: Easing.out(Easing.exp)
-                    });
-            }}
-        />
-    }
+    // constructor(props) {
+    //     super(props);
+    //     var ds = new ViewPager.DataSource({rowHasChanged: (r1, r2) => r1 !== r2});
+    //     this.state = {
+    //         dataSource: ds.cloneWithRows(this._genRows()),
+    //     };
+    // }
+    //
+    // _genRows() {
+    //     const dataBlob = [];
+    //     for (let i = 0; i < 5; i++) {
+    //         dataBlob.push("aa" + i);
+    //     }
+    //     return dataBlob;
+    // }
+    //
+    // render() {
+    //     return
+    //     // <ViewPager
+    //     //     dataSource={this.state.dataSource}
+    //     //     renderPage={this._renderPage}
+    //     //     animation={(animatedValue, toValue, gestureState) => {
+    //     //         // Use the horizontal velocity of the swipe gesture
+    //     //         // to affect the length of the transition so the faster you swipe
+    //     //         // the faster the pages will transition
+    //     //         var velocity = Math.abs(gestureState.vx);
+    //     //         var baseDuration = 300;
+    //     //         var duration = (velocity > 1) ? 1 / velocity * baseDuration : baseDuration;
+    //     //
+    //     //         return Animated.timing(animatedValue,
+    //     //             {
+    //     //                 toValue: toValue,
+    //     //                 duration: duration,
+    //     //                 easing: Easing.out(Easing.exp)
+    //     //             });
+    //     //     }}
+    //     // />
+    // }
 }
